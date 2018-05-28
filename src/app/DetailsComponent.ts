1433bcc2171b384fdf9c279424ac194ecc0c7a2e
Igor Nadiein <snoozzzy7@gmail.com>
2018-05-28 22:13:40 +0300

    AT:
    Added table and details components;

---
+++ src/app/DetailsComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './DetailsComponent.html',
+    styleUrls: ['./DetailsComponent.css']
+})
+export class DetailsComponent {
+    title = 'details component';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { TableComponent } from './TableComponent';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        TableComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [TableComponent]
 })
 export class AppModule { }
